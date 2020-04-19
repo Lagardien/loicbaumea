f21f6d57969a9664c0696ef76e2f4b1a2f0d3363
Loïc Bauméa <lagardien26@gmail.com>
2020-04-19 17:35:26 +0100

    new transition

--- js/transition.js
+++ js/transition.js
@@ -4,6 +4,8 @@ import Highway from "@dogstudio/highway";
 // GSAP Library
 import Tween from "gsap";
 
+const $frameBlack = document.querySelector(".left-border");
+
 // Fade
 class Fade extends Highway.Transition {
   in({ from, to, done }) {
@@ -15,12 +17,14 @@ class Fade extends Highway.Transition {
 
     // Animation
     Tween.fromTo(
-      to,
-      0.5,
-      { opacity: 0 },
+      $frameBlack,
+      1,
+      { width: "100vw" },
       {
-        opacity: 1,
-        onComplete: done
+        width: "50px",
+        transformOrigin: "left",
+        ease: Power4.easeInOut,
+        onComplete: done,
       }
     );
   }
@@ -28,12 +32,14 @@ class Fade extends Highway.Transition {
   out({ from, done }) {
     // Animation
     Tween.fromTo(
-      from,
-      0.5,
-      { opacity: 1 },
+      $frameBlack,
+      1.5,
+      { width: "50px" },
       {
-        opacity: 0,
-        onComplete: done
+        width: "100vw",
+        transformOrigin: "left",
+        ease: Power4.easeInOut,
+        onComplete: done,
       }
     );
   }
